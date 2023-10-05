2efb268469d25e9c62c16561b95cfbdddd4ab138
Tonesha Rose <131703989+tonesharose31@users.noreply.github.com>
2023-10-05 01:44:59 -0400

    created logic in app.js for hompage and displays msg and link

--- app.js
+++ app.js
@@ -1,9 +1,16 @@
 const express = require("express");
 const app = express();
 
-app.get("/", (req,res) => {
-  res.send("Hello World")
-})
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+app.get("/bugs", (req,res) => {
+    const bugsLeft = 99;
+    const linkText = "pull one down, patch it around"
+  res.send(`<p>${bugsLeft} little bugs in the code</p>
+  <a href='/bugs/${bugsLeft + 2}'>${linkText}</a>`);
+});
 
 
 
