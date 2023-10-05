265c3c562eb968ce99857b63e8fe8717088e6e0d
Michael Kleemoff <michaelkleemoff2@gmail.com>
2023-10-04 22:28:29 -0400

    Add 'New Project Name Generator' route.

--- app.js
+++ app.js
@@ -10,5 +10,13 @@ app.get('/', (req, res) => {
 	res.send('<h1>Welcome 🖖 to 99 Little Bugs 🪰🪲🐛 & Poke-Express 🔴⚪️</h1>');
 });
 
+// New Project Name Generator
+app.get('/:verb/:adjective/:noun', (req, res) => {
+	const { verb, adjective, noun } = req.params;
+	res.send(
+		`Congratulations on starting a new project called ${verb}-${adjective}-${noun}!`
+	);
+});
+
 // EXPORT
 module.exports = app;
