c0f517886a72fcb2ad3552003cb4bc249c697951
arim83 <jorammercado@pursuit.org>
2023-10-05 06:18:31 -0400

    reformat to pass tests

--- app.js
+++ app.js
@@ -1,13 +1,9 @@
 const express = require("express")
 const app = express()
+const pokemon = require("./models/pokemon.json")
 
 app.get("/", (req,res)=>{
-    res.send("Hello World!!!")
-})
-
-app.get("/:verb/:adj/:noun", (req,res)=>{
-    res.send(`Congratulations on starting a new project called `+
-             `${req.params.verb}-${req.params.adj}-${req.params.noun}!`)
+    res.send("Welcome 99 Pokemon")
 })
 
 app.get("/bugs", (req,res)=>{
@@ -15,7 +11,7 @@ app.get("/bugs", (req,res)=>{
     res.send(`${num} little bugs in the code`+ `<br /><br />`+
              `${num} little bugs <br />`+
              `<a style="text-decoration:none;" href="/bugs/101">`+
-             `pull one down<br />patch it around</a><br />`+
+             `Pull one down, patch it around</a><br />`+
              `${num+2} bugs in the code`)
 })
 
@@ -23,15 +19,11 @@ app.get("/bugs/:number", (req,res)=>{
     const num = Number(req.params.number) 
     let href = ""
     let partingMessage = ""
-    if(num+2>200){
+    if(num>=200){
         href = "/bugs"
-        partingMessage = `${num} little bugs in the code`+ `<br /><br />`+
-                         `${num} little bugs<br />`+
-                         `pull one down<br/ >patch it around</a><br />`+
-                         `${num+2} bugs in the code<br />` +
-                         `<a style="text-decoration:none;"`+
+        partingMessage = `<a style="text-decoration:none;"`+
                          ` href="${href}">`+
-                         `start over<br /><br />`
+                         `Too many bugs!! Start over!</a>`
     }
     else{
     href= `/bugs/${num+2}`
@@ -39,10 +31,19 @@ app.get("/bugs/:number", (req,res)=>{
                      `${num} little bugs<br />`+
                      `<a style="text-decoration:none;"`+
                      ` href="${href}">`+
-                     `pull one down<br/ >patch it around</a><br />`+
+                     `Pull one down, patch it around</a><br />`+
                      `${num+2} bugs in the code`   
     }
     res.send(partingMessage)   
+})
+
+app.get("/pokemon", (req, res) => {
+    res.send(pokemon)
+})
+
+app.get("/:verb/:adj/:noun", (req,res)=>{
+    res.send(`Congratulations on starting a new project called `+
+             `${req.params.verb}-${req.params.adj}-${req.params.noun}!`)
 })
 
 module.exports = app
