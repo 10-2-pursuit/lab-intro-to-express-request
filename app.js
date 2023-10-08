93296ef6e0c5ea3a793cb0acad3f9e4bf7b1733d
AishaCKleemoff <96323559+AishaCKleemoff@users.noreply.github.com>
2023-10-08 18:14:20 -0400

    Submit lab, work in progress.

--- app.js
+++ app.js
@@ -9,5 +9,20 @@ app.get("/", (req, res) => {
   res.send("🐞Welcome to Eric and Aisha's Express app🦋");
 });
 
+app.get("/:verb/:adjective/:noun", (req, res) => {
+  const { verb, adjective, noun } = req.params;
+  res.send(
+    `Congratulations on starting a new project called  ${verb}-${adjective}-${noun}`
+  );
+});
+
+app.get("/bugs", (req, res) => {
+  res.send(`🪲99 little bugs in the code🐜`);
+});
+
+app.get("/bugs", (req, res) => {
+  res.send(`🐞99 little bugs in the code🦋`);
+});
+
 //EXPORT
 module.exports = app;
