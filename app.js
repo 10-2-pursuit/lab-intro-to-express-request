9ea28397608e7e6c781fc20790e0e1a92c9f551a
Shanice <shanicegriffin@pursuit.org>
2023-10-08 15:03:59 -0400

    passed 5 tests

--- app.js
+++ app.js
@@ -2,43 +2,51 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("Welcome!");
+  res.send("Welcome 99 Pokemon");
 });
 
 app.get("/:verb/:adjective/:noun", (req, res) => {
   const { verb, adjective, noun } = req.params;
   const projectName = `${verb}-${adjective}-${noun}`;
-  res.send(`Congratulations on starting a new project called ${projectName}`);
+  res.send(`Congratulations on starting a new project called ${projectName}!`);
 });
 
-let numberOfBugs = 99;
-
 app.get("/bugs", (req, res) => {
+  let numberOfBugs = 99;
   const message = `${numberOfBugs} little bugs in the code`;
   const link = "pull one down, patch it around";
-  res.send(`${message} <br> <a href="/bugs/${numberOfBugs + 10}"> ${link} <a/>`);
+  res.send(`${message} <br> <a href="/bugs/${numberOfBugs + 2}"> ${link} </a>`);
 });
 
 app.get("/bugs/:numberOfBugs", (req, res) => {
   const { numberOfBugs } = req.params;
-  const bugs = parseInt(numberOfBugs);
+  const bugs = Number(numberOfBugs);
 
-  if (numberOfBugs > 200) {
-    const link = "start over"
-    res.send(`<a href="/bugs"> ${link} <a/>`);
-  } else {
+  if (numberOfBugs < 200) {
     const message = `${bugs} little bugs in the code`;
     const link = "pull one down, patch it around";
-    res.send(`${message} <br> <a href="/bugs/${bugs + 10}"> ${link} <a/>`);
+    res.send(`${message} <br> <a href=/bugs/"${bugs + 2}"> ${link} </a>`);
+  } else {
+    const link = "Too many bugs!! Start over!";
+    res.send(`<a href="/bugs"> ${link} </a>`);
   }
 });
 
 const pokemon = require("./models/pokemon.json");
 
 app.get("/pokemon", (req, res) => {
-    const pokemonNames = pokemon.map(poke => poke.name);
-    const pokemonList = pokemonNames.map(name => `<li>${name}</li>`).join("");
-    res.send(`<ul>${pokemonList}</ul>`)
-})
+  res.json(pokemon);
+});
+
+app.get("/pokemon/search", (req, res) => {
+  const { name } = req.query;
+  const foundPokemon = pokemon.filter(poke => poke.name.toLowerCase() === name.toLocaleLowerCase());
+
+  if(foundPokemon.length > 0) {
+    res.json(foundPokemon);
+  } else {
+    res.json([]);
+  }
+});
 
 module.exports = app;
